d5264ca64c98f4f4b2ad507a3f2a62ef59119770
Arlisson <Arlissonjaques@gmail.com>
2020-09-26 11:21:52 -0300

    Exercicio 8 adicionado

--- exercicios/src/Main.java
+++ exercicios/src/Main.java
@@ -1,14 +1,8 @@
-import entra.processamento.saida.Exer1;
-import entra.processamento.saida.Exer2;
-import entra.processamento.saida.Exer3;
-import entra.processamento.saida.Exer4;
-import entra.processamento.saida.Exer5;
-import entra.processamento.saida.Exer6;
-import entra.processamento.saida.Exer7;
+import entra.processamento.saida.Exer8;
 
 public class Main {
 
     public static void main(String[] args) {
-        Exer7.exercicio7();
+        Exer8.exercicio8();
     }
 }
