a23f77dc97d95fafe81910e88f6ff4175db1f2d9
Arlisson Jaques <arlissonjaques@gmail.com>
2020-09-25 20:16:44 -0500

    Adicionado exercicio 4

--- exercicios/src/Main.java
+++ exercicios/src/Main.java
@@ -1,10 +1,11 @@
 import entra.processamento.saida.Exer1;
 import entra.processamento.saida.Exer2;
 import entra.processamento.saida.Exer3;
+import entra.processamento.saida.Exer4;
 
 public class Main {
 
     public static void main(String[] args) {
-        Exer3.exercicio3();
+        Exer4.Exercicio4();
     }
 }
