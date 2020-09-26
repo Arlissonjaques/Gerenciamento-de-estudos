d87dd48d6198f84d892affe2d20c1f7535ba9645
Arlisson <Arlissonjaques@gmail.com>
2020-09-26 09:53:46 -0300

    Exercicio 7 adicionado

--- exercicios/src/Main.java
+++ exercicios/src/Main.java
@@ -4,10 +4,11 @@ import entra.processamento.saida.Exer3;
 import entra.processamento.saida.Exer4;
 import entra.processamento.saida.Exer5;
 import entra.processamento.saida.Exer6;
+import entra.processamento.saida.Exer7;
 
 public class Main {
 
     public static void main(String[] args) {
-        Exer6.exercicio6();
+        Exer7.exercicio7();
     }
 }
---
+++ exercicios/src/entra/processamento/saida/Exer7.java
@@ -0,0 +1,23 @@
+package entra.processamento.saida;
+
+import java.util.Scanner;
+
+public class Exer7 {
+/*
+    Ler uma temperatura em graus Fahrenheit e apresentá-Ia convertida em graus Celsius. A fórmula de conversão de
+    temperatura a ser utilizada é C = (F - 32) * 5 / 9, em que a variável F é a temperatura em graus Fahrenheit e a
+    variável C é a temperatura em graus Celsius.
+
+*/
+    public static void exercicio7(){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite a temperatura em ºF a ser convertida: ");
+        int fahrenheit = input.nextInt();
+
+        int celsius = (fahrenheit - 32) * 5 / 9;
+
+        System.out.printf("%d fahrenheit são %d celsius", fahrenheit, celsius);
+
+    }
+}
