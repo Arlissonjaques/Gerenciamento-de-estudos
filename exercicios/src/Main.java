afd99771715d5a26c44ec2b6a2f82f1773f5184b
Arlisson Jaques <arlissonjaques@gmail.com>
2020-10-03 16:51:36 -0500

    Exercicio 14 adicionado

--- exercicios/src/Main.java
+++ exercicios/src/Main.java
@@ -1,8 +1,8 @@
-import page2.Exer13;
+import page2.Exer14;
 
 public class Main {
 
     public static void main(String[] args) {
-        Exer13.exercicio13();
+        Exer14.exercicio14();
     }
 }
